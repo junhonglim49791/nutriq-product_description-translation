516291aea98634215a30a4822ffd45a435b2f7af
junhonglim49791 <limjunhong4979@gmail.com>
2025-06-21 15:49:38 +0800

    Parse html of 1 row

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from bs4 import BeautifulSoup
+from googletrans import Translator
 
-df = pd.read_excel("product_description.xlsx", header=0, skiprows=[1,2,3])
-print(df)
+
+df = pd.read_excel("product_description_ubuntu.xlsx", header=0, skiprows=[1,2,3])
+# print(df.iloc[0]["Main Description"])
+
+soup = BeautifulSoup(df.loc[0, "Main Description"], 'html.parser')
+
+print(soup.prettify())
